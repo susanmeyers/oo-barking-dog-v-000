e242cca870d6260ebae908319525cd770cfbf306
Susan Meyers <smmeyers24@gmail.com>
2018-07-20 23:11:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+
+  class Dog
+  attr_accessor :name
+
+  def initiatlize(name= "Fido")
+  @name= name
+  end
+
+  def name(name)
+    @name= name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+     puts "woof!"
+   end
+
+
+  fido = Dog.new
+  fido.name
+  fido.bark
+
+
+end
